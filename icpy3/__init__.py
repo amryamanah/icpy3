ca6431d5d5e75a34e7bc690373d10c12b91e7ce4
amryfitra <amryfitra@gmail.com>
2015-05-16 17:32:15 +0900

    add accessor to IC_Exception

--- icpy3/__init__.py
+++ icpy3/__init__.py
@@ -1,6 +1,7 @@
 __author__ = 'amryf'
 
 from .ic_imaging_control import IC_ImagingControl
+from .ic_exception import IC_Exception
 
 IC_ImagingControl = IC_ImagingControl
-
+IC_Exception = IC_Exception
